0224c1b7d1b866debcc4a4d7878ce52ef3e03369
LeoAlfaroC <leonardo14512+upc@gmail.com>
2020-09-28 22:29:24 -0500

    Increased max file size in php config

--- html/app/Http/Controllers/SongController.php
+++ html/app/Http/Controllers/SongController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Responses\APIResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 class SongController extends Controller
